1f55e35a31300cee36ce04e7de01b085c3dc6070
kazungu <kazungu61@gmail.com>
2019-06-20 19:35:29 +0300

    SELECT data from database using array and while loop

--- lesson_3/Databases/create_table.php
+++ lesson_3/Databases/create_table.php
@@ -4,6 +4,7 @@ $conn = new mysqli('localhost','root','','mit_evening');
 if ($conn ->connect_error){
     die("connection failed ". $conn->connect_error);
 }
+/*
 $sql = 'CREATE TABLE student(
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(200) NOT NULL,
@@ -19,10 +20,38 @@ if($conn->query($sql)=== TRUE){
 }
 
 $sql = "INSERT INTO student(firstname,lastname,reg_date)
-        VALUES('Terry', 'Mukiri',NULL);";
+        VALUES('Rose', 'Njenga',NULL);";
+/*
 if($conn->query($sql) === TRUE){
     echo "new data entered successfully";
 }else{
     echo "Error: ".$sql. "<br>". $conn->error;
 }
+if(mysqli_query($conn,$sql)){
+    #get last item added into the database.
+    $last_item = mysqli_insert_id($conn);
+    echo "New Record added successfully";
+    echo "record is : $last_item";
+}else{
+    echo "Error".$sql.mysqli_error($conn)."<br>";
+}*/
+//selecting data from a table(s)
+//SELECT column_one, column_two FROM tablename
+//SELECT * FROM tablename
+
+$sql = "SELECT id, firstname,lastname FROM student";
+$result = mysqli_query($conn,$sql);
+//associative array key and value
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . " ". $row['id']." ". $row['firstname']. " ". $row['lastname']."<br>";
+    }
+
+}else{
+    echo "no record found";
+}
+
+
+
+
 mysqli_close($conn);
