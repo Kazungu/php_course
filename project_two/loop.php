5433ec2845245e30dfdd9f3364d159366ba7b05a
kazungu <kazungu61@gmail.com>
2019-06-24 12:44:19 +0300

    CRUD

--- project_two/loop.php
+++ project_two/loop.php
@@ -1,15 +1,18 @@
 <?php
-$result = "SELECT * FROM `product`";
-$fetchResults = mysqli_query($conn,$result);
-
-while($row = mysqli_fetch_assoc($fetchResults)){
+//query/request
+$results = "SELECT * FROM `product`";
+#store the result/gragg the result
+$fetchResutls = mysqli_query($conn, $results);
+while ($row = mysqli_fetch_assoc($fetchResutls)){
     extract($row);
-    echo "     <tr>
-                <td>$id</td>
-                <td>$name</td>
-                <td>$size</td>
-                <td>$price</td>
-                <td><a href='#' class='btn btn-danger'>Delete</a> </td>
-                <td><a href='#' class='btn btn-info'>Update</a> </td>
-               </tr>";
+    echo "<tr>
+            <td>$id</td>
+            <td>$name</td>
+            <td>$size</td>
+            <td>$price</td>
+            <td><a href='#' class='btn btn-danger'>Delete</a></td>
+            <td><a href='#' class='btn btn-info'>Update</a></td>
+            
+           </tr>";
 }
+?>
